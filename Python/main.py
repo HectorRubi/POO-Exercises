f2f52931377152f82d923116be2e116a673232c3
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-08-29 12:48:53 -0500

    Add constructor methods in Java and JavaScript and working with objects

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
-  print("Hola mundo");
+  print("Hola mundo")
+
+  car = Car()
+  car.licence = "AMS234"
+  car.driver = "Andres Herrera"
+  print(vars(car))
+
+  car2 = Car()
+  car2.licence = "QWE567"
+  car2.driver = " Andrea Herrera"
+  print(vars(car2))
