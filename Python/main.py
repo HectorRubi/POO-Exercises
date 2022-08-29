8ca6611cf2ed1d453d7931210cf5fca4d4289e2a
Hector Rubi Linuxero <hector.rubi.garcia@outlook.com>
2022-08-29 13:14:50 -0500

    Add constructor methods in Python

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,13 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
   print("Hola mundo")
 
-  car = Car()
-  car.licence = "AMS234"
-  car.driver = "Andres Herrera"
+  car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
   print(vars(car))
+  print(vars(car.driver))
 
-  car2 = Car()
-  car2.licence = "QWE567"
-  car2.driver = " Andrea Herrera"
+  car2 = Car("QWE567", Account("Andrea Herrera", "AND123"))
   print(vars(car2))
+  print(vars(car2.driver))
